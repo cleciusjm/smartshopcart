553aff83d60990520cb41382472a3acd91a486c0
Clécius J. Martinkoski <clecius@e-precise.com.br>
2018-03-26 23:49:00 -0300

    Implementação inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { ListComponent } from './list/list.component';
+import { CovalentLayoutModule } from '@covalent/core';
+import { MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    CovalentLayoutModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
